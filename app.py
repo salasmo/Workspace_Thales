dbc0523c948b94fde9fdaf5434aac047167946bf
salasmo <santicuv@gmail.com>
2025-10-20 17:48:15 -0600

    RESTAURATION

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from pages import dashboard, map
 # App configuration
 # ===============================
 st.set_page_config(
-    page_title="CDMX Crime Analytics Dashboard V2.0",
+    page_title="CDMX Crime Analytics Dashboard",
     layout="wide",
     page_icon="📊"
 )
